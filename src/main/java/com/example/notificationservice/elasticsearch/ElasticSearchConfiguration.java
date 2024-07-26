2d281d17e0662702984ed9b9ca49079eb23b4a56
Sreehari T Rajesh <sreehari.rajesh@meesho.com>
2024-07-26 17:00:57 +0530

    added configuration for elastic-search

---
+++ src/main/java/com/example/notificationservice/elasticsearch/ElasticSearchConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.notificationservice.elasticsearch;
+
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+import javax.net.ssl.SSLContext;
+
+@Configuration
+public class ElasticSearchConfiguration extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedToLocalhost()
+                .usingSsl(buildSslContext())
+                .withBasicAuth("elastic", "changeme")
+                .build();
+    }
+
+    private static SSLContext buildSslContext() {
+        try {
+            return new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
