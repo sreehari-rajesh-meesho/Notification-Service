5b880428d64f5983f58690447c9ebaefafa3b718
Sreehari T Rajesh <sreehari.rajesh@meesho.com>
2024-07-26 16:09:00 +0530

    added code for try-catch exception handling

--- src/main/java/com/example/notificationservice/constants/Constants.java
+++ src/main/java/com/example/notificationservice/constants/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
     public static final long INGESTION_SUCCESSFUL = 0;
     public static final long UPDATE_SUCCESSFUL = 0;
 
+    public static final long DATABASE_ERROR = -1;
     // Failure Codes
-    public static final long PHONE_NUMBER_MANDATORY = -1;
-    public static final long MESSAGE_WITH_ID_NOT_FOUND = -2;
-
+    public static final long PHONE_NUMBER_MANDATORY = -2;
+    public static final long MESSAGE_WITH_ID_NOT_FOUND = -3;
 
 }
--- src/main/java/com/example/notificationservice/message/MessageService.java
+++ src/main/java/com/example/notificationservice/message/MessageService.java
@@ -1,10 +1,9 @@
 package com.example.notificationservice.message;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.notificationservice.constants.Constants.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static com.example.notificationservice.constants.Constants.*;
@@ -14,9 +13,7 @@ import static com.example.notificationservice.constants.Constants.*;
 @Service
 public class MessageService {
 
-
         private final MessageRepository messageRepository;
-
 
         public Long IngestMessageToDatabase(Message message) {
 
@@ -27,24 +24,39 @@ public class MessageService {
                         return PHONE_NUMBER_MANDATORY;
                 }
 
-                Message savedMessage = messageRepository.save(message);
-                Long messageId = savedMessage.getId();
-                return messageId;
+                message.setCreated_at(LocalDateTime.now());
+
+                try {
+                        Message savedMessage = messageRepository.save(message);
+                        Long messageId = savedMessage.getId();
+                        return messageId;
+
+                } catch (Exception e) {
+                        return DATABASE_ERROR;
+                }
         }
 
         public Long UpdateMessageInDatabase(Long message_id, Integer status, String failure_code, String failure_comments) {
 
-                Optional<Message> message = messageRepository.findById(message_id);
+                try {
+                        Optional<Message> message = messageRepository.findById(message_id);
 
-                if(message.isPresent()) {
-                        message.get().setStatus(status);
-                        message.get().setFailure_code(failure_code);
-                        message.get().setFailure_comments(failure_comments);
-                        messageRepository.save(message.get());
-                        return UPDATE_SUCCESSFUL;
-                }
-                else {
-                        return MESSAGE_WITH_ID_NOT_FOUND;
+                        if(message.isPresent()) {
+
+                                message.get().setStatus(status);
+                                message.get().setFailure_code(failure_code);
+                                message.get().setFailure_comments(failure_comments);
+                                message.get().setUpdated_at(LocalDateTime.now());
+
+                                messageRepository.save(message.get());
+
+                                return UPDATE_SUCCESSFUL;
+                        }
+                        else {
+                                return MESSAGE_WITH_ID_NOT_FOUND;
+                        }
+                } catch (Exception e) {
+                        return DATABASE_ERROR;
                 }
         }
 }
