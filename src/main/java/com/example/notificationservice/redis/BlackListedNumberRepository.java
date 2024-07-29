2468dcbaa2e63485938971f3a53d02ae2971bf02
Sreehari T Rajesh <sreehari.rajesh@meesho.com>
2024-07-29 12:15:16 +0530

    added blacklisted number

--- src/main/java/com/example/notificationservice/redis/BlackListedNumberRepository.java
+++ src/main/java/com/example/notificationservice/redis/BlackListedNumberRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlackListedNumberRepository extends CrudRepository<BlackListedNumber, Long> {
+public interface BlackListedNumberRepository extends CrudRepository<BlackListedNumber, String> {
 
 }
